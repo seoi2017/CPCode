9a187eedce6f855e2f4cfef119060b65cef33db4
seoi2017 <iaswork@foxmail.com>
2021-02-02 10:36:38 +0800

    Init Submit

---
+++ Contest/PTZ_ICPC_Camp/Day1/A.cc
@@ -0,0 +1,22 @@
+/*
+Author: Invrise
+For Archive Only, Please Do NOT Copy.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifdef LOCAL
+    freopen("in.io", "r", stdin);
+    freopen("out.io", "w", stdout);
+#endif
+    int a, b;
+    cin >> a >> b;
+    int c = a + b;
+    cout << c << endl;
+#ifdef LOCAL
+    fclose(stdin);
+    fclose(stdout);
+#endif
+    return 0;
+}
